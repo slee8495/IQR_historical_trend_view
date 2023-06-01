0111fd17e8d27fa7d7374231798b6e3c74525770
slee <sanlee8495@gmail.com>
2023-05-31 23:39:47 -0700

    5/31/2023

---
+++ IQR historical data view.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(magrittr)
+library(openxlsx)
+library(readxl)
+library(writexl)
+library(reshape2)
+library(skimr)
+library(janitor)
+library(lubridate)
+
+
+############ Data collection ###############
+
+data_1 <- read_excel("S:/Supply Chain Projects/LOGISTICS/SCP/Cost Saving Reporting/Inventory Days On Hand/IQR archive/",
+                     sheet = )
+
+
+# Currently, need to work on data folder work first..
+# FG, RM.. 
+
